f1b1bfa0fa50d998e736b07e90b60a42f8c0ef03
eddy zapata <ecz001@live.com>
2024-01-17 16:21:45 -0500

    adding transition to drawer

--- src/components/drawers/LeftDrawer.tsx
+++ src/components/drawers/LeftDrawer.tsx
@@ -9,10 +9,12 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Slide,
 } from '@mui/material';
 import { causes as Causes } from './causes';
 import InboxIcon from '@mui/icons-material/Inbox';
 import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
+import Scrollbars from 'react-custom-scrollbars';
 
 interface CausesListProps {
   open: boolean;
@@ -43,22 +45,7 @@ const LeftDrawer: React.FC = () => {
   const handleDrawer = () => {
     setOpen(!open);
   };
-  // should change to use styled components here for drawer
   return (
-    // <Drawer
-    //   variant='permanent'
-    //   open={open}
-    //   PaperProps={{ height: 100, width: 100, bgcolor: 'red' }}
-    //   ModalProps={{ sx: { height: 100 } }}
-    // >
-    //   {/* <Button onClick={() => handleDrawer()} sx={{ pt: 10, position: 'fixed' }}>
-    //     <VolunteerActivismIcon fontSize='large' />
-    //     {open && <Typography variant='h6'>Causes</Typography>}
-    //   </Button> */}
-    //   <Box sx={{ width: open ? 200 : 0, height: '100px' }}>
-    //     {/* <CausesList open={false} causes={Causes} /> */}
-    //   </Box>
-    // </Drawer>
     <>
       <Button
         onClick={() => handleDrawer()}
@@ -72,24 +59,29 @@ const LeftDrawer: React.FC = () => {
         }}
       >
         <VolunteerActivismIcon fontSize='large' />
-        {open && <Typography variant='h6'>Causes</Typography>}
+        <Slide direction='right' in={open} mountOnEnter unmountOnExit>
+          <Typography variant='h6'>Causes</Typography>
+        </Slide>
       </Button>
-      <Box
-        width={open ? 236 : '0%'}
-        height={'80%'}
-        bgcolor={'red'}
-        overflow={'auto'}
-        sx={{
-          position: 'fixed',
-          zIndex: 1000,
-          top: 116,
-          left: '1%',
-          borderRadius: '25px 25px 25px 25px',
-          bgcolor: '#F0F8FF',
-        }}
-      >
-        <CausesList open={open} causes={Causes} />
-      </Box>
+      <Slide direction='right' in={open} mountOnEnter unmountOnExit>
+        <Box
+          width={236}
+          height={'80%'}
+          overflow={'auto'}
+          sx={{
+            position: 'fixed',
+            zIndex: 1000,
+            top: 116,
+            left: '1%',
+            borderRadius: '25px 25px 25px 25px',
+            bgcolor: '#F0F8FF',
+          }}
+        >
+          <Scrollbars style={{ width: '100%', height: '100%' }}>
+            <CausesList open={open} causes={Causes} />
+          </Scrollbars>
+        </Box>
+      </Slide>
     </>
   );
 };
