b3fd6756ceff86db5db82a3fd38b1aa2c179b440
eddy zapata <ecz001@live.com>
2024-01-22 23:35:48 -0500

    adding hover effect on cards

--- src/components/organizationCard/OrganizationCard.tsx
+++ src/components/organizationCard/OrganizationCard.tsx
@@ -12,8 +12,77 @@ import {
 } from '@mui/material';
 import StockImage from '../../assets/charity-8366471_1280.png';
 interface OrganizationCardProps {
-  data: unknown[];
+  data?: unknown[];
+  children?: React.ReactNode;
 }
+interface HoverCardProps {
+  children: React.ReactNode;
+  name: string;
+  slug: string;
+  logoUrl: string;
+  coverImageUrl: string;
+  description: string;
+}
+const HoverCard: React.FC<HoverCardProps> = ({
+  children,
+  name,
+  slug,
+  logoUrl,
+  coverImageUrl,
+  description,
+}) => {
+  const [hover, setHover] = React.useState(false);
+  const handleMouseEnter = () => {
+    setHover(!hover);
+  };
+  function createWidget(slug: string) {
+    /* tslint:disable-next-line */
+    // @ts-expect-error this will work once loaded
+    everyDotOrgDonateButton?.createButton({
+      selector: `#every-donate-${slug}`,
+    });
+    /* tslint:disable-next-line */
+    // @ts-expect-error this will work once loaded
+    everyDotOrgDonateButton?.createWidget({
+      selector: `#every-donate-${slug}`,
+      nonprofitSlug: slug,
+      //   nonprofitSlug: 'lilbubsbigfund',
+    });
+  }
+  setTimeout(() => {
+    createWidget(slug);
+    console.log(slug, 'slug');
+  }, 0);
+  return (
+    <Card
+      onMouseEnter={() => {
+        console.log('hovered');
+        handleMouseEnter();
+      }}
+      onMouseLeave={() => {
+        console.log('hovered');
+        handleMouseEnter();
+      }}
+      sx={{ position: 'relative', height: 400 }}
+    >
+      <CardMedia
+        component={'img'}
+        height={140}
+        image={coverImageUrl ? coverImageUrl : StockImage}
+      />
+      <CardHeader
+        title={name}
+        subheader={slug}
+        avatar={<img src={logoUrl} />}
+        sx={{ height: 32 }}
+      />
+      <CardContent sx={{ overflowY: 'auto', height: 114 }}>
+        <Typography>{description}</Typography>
+      </CardContent>
+      <CardActions sx={{ height: 40, justifyContent: 'flex-end' }}>{hover && children}</CardActions>
+    </Card>
+  );
+};
 
 const OrganizationCard: React.FC<OrganizationCardProps> = ({ data }) => {
   function createWidget(slug: string) {
@@ -33,11 +102,11 @@ const OrganizationCard: React.FC<OrganizationCardProps> = ({ data }) => {
 
   return (
     <>
-      {data.map((item: any, index: number) => {
-        setTimeout(() => {
-          createWidget(item.slug);
-          console.log(item.slug, 'slug');
-        }, 0);
+      {data?.map((item: any, index: number) => {
+        // setTimeout(() => {
+        //   createWidget(item.slug);
+        //   console.log(item.slug, 'slug');
+        // }, 0);
         console.log(item, 'item');
         return (
           <Grow
@@ -47,32 +116,32 @@ const OrganizationCard: React.FC<OrganizationCardProps> = ({ data }) => {
             key={item.name}
           >
             <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-              <Card sx={{ position: 'relative', height: 1 }}>
-                <CardMedia
-                  component={'img'}
-                  height={'140'}
-                  image={item && item.coverImageUrl ? item.coverImageUrl : StockImage}
-                />
-                <CardHeader
-                  title={item.name}
-                  subheader={item.slug}
-                  avatar={<img src={item.logoUrl} />}
-                />
-                <CardContent sx={{ overflowY: 'auto' }}>
-                  <Typography>{item.description}</Typography>
-                </CardContent>
-                <CardActions sx={{ height: 40 }}>
-                  <Button
-                    id={`every-donate-${item.slug}`}
-                    // sx={{ bgcolor: 'red' }}
-                    href={`https://www.every.org/${item.slug}#/donate`}
-                    sx={{ position: 'absolute', bottom: '2%', right: '2%' }}
-                  >
-                    {/* <a href={`https://www.every.org/${item.slug}#/donate`}>Donate</a> */}
-                    Donate
-                  </Button>
-                </CardActions>
-              </Card>
+              <HoverCard
+                name={item.name}
+                slug={item.slug}
+                logoUrl={item.logoUrl}
+                coverImageUrl={item.coverImageUrl}
+                description={item.description} // sx={{ position: 'relative', height: 1 }}
+                // onClick={() => {
+                //   console.log('clicked');
+                // }}
+                // onMouseEnter={() => {
+                //   console.log('hovered');
+                // }}
+                // onMouseOver={() => {
+                //   console.log('hovered');
+                // }}
+              >
+                <Button
+                  id={`every-donate-${item.slug}`}
+                  // sx={{ bgcolor: 'red' }}
+                  href={`https://www.every.org/${item.slug}#/donate`}
+                  sx={{ position: 'relative', bottom: '2%', right: '2%' }}
+                >
+                  {/* <a href={`https://www.every.org/${item.slug}#/donate`}>Donate</a> */}
+                  Donate
+                </Button>
+              </HoverCard>
             </Grid>
           </Grow>
         );
