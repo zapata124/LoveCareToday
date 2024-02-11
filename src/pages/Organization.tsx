59b3b3856ff53bbe4eecb3453fb259d8faa509bf
eddy zapata <ecz001@live.com>
2024-02-10 23:12:13 -0500

    adding prefetch to organization

--- src/pages/Organization.tsx
+++ src/pages/Organization.tsx
@@ -5,19 +5,27 @@ import { useParams } from 'react-router-dom';
 import { useUpdatePage } from '../providers/PageProvider';
 import OrganizationCard from '../components/organizationCard';
 import { RenderSearchSkeleton } from '../components/skeletons';
+import { client } from '../client';
 const removeDash = (str: string) => {
   return str.replace(/-/g, ' ');
 };
 
 const Organization: React.FC = () => {
   const { id } = useParams();
-  const { page, handleUpdatePage, handleTotalPages } = useUpdatePage();
+  const { page, totalPages, handleUpdatePage, handleTotalPages } = useUpdatePage();
   // adding to lower case to fix api error
   const parsedId = removeDash(id || '').toLowerCase();
   const { loading, error, data } = useQuery(getNonProfitByCause, {
     variables: { browse: parsedId, take: 20, page: page },
   });
-  console.log(data);
+  useEffect(() => {
+    if (page && page !== totalPages) {
+      client.query({
+        query: getNonProfitByCause,
+        variables: { browse: parsedId, take: 20, page: page + 1 },
+      });
+    }
+  }, [page]);
   useEffect(() => {
     if (data?.cause?.pagination?.pages) {
       handleTotalPages(data.cause.pagination.pages);
