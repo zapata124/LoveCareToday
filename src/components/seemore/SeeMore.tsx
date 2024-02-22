68f44c7e40249800c495b55a67e61a1fbca0c3a1
eddy zapata <ecz001@live.com>
2024-02-22 16:35:53 -0500

    adding new address component

--- src/components/seemore/SeeMore.tsx
+++ src/components/seemore/SeeMore.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { Children, ReactNode, useEffect, useMemo, useRef, useState } from 'react';
 import {
   Avatar,
   Card,
@@ -19,6 +19,8 @@ import {
   Box,
   Link,
   Divider,
+  Tabs,
+  Tab,
 } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
@@ -37,6 +39,85 @@ const FallBackState: React.FC<Children> = ({ children }) => {
   );
 };
 
+const SeemoreTabs: React.FC<Children> = ({ children }) => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Tabs value={value} onChange={handleChange} aria-label='secondary tabs example'>
+        <Tab value={0} label='Location' />
+        <Tab value={1} label='Finance' />
+        <Tab value={2} label='IRS' />
+      </Tabs>
+      {Children.toArray(children)[value]}
+    </Box>
+  );
+};
+
+interface PartialLocationProp {
+  address: string | undefined;
+}
+const PartialLocation: React.FC<PartialLocationProp> = ({ address }) => {
+  return (
+    <Stack direction={'column'}>
+      <Stack direction={'row'} alignItems={'center'} spacing={1}>
+        <Typography sx={{ fontWeight: 600 }}>Location</Typography>
+        <LocationOnIcon />
+      </Stack>
+      <Typography sx={{ mb: 2 }}>{address}</Typography>
+    </Stack>
+  );
+};
+interface FullLocationProps {
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  country: string;
+  assetAmount: string;
+  form990: string;
+}
+const FullLocation: React.FC<Omit<FullLocationProps, 'assetAmount' | 'form990'>> = ({
+  street,
+  city,
+  state,
+  zipCode,
+  country,
+}) => {
+  return (
+    <Grid container>
+      <Grid container item xs={12} sm={6}>
+        <Grid item xs={12}>
+          <Typography sx={{ fontWeight: 600, fontSize: '16px' }}>Street</Typography>
+          <Typography>{street}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography sx={{ fontWeight: 600, fontSize: '16px' }}>City</Typography>
+          <Typography>{city}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography sx={{ fontWeight: 600, fontSize: '16px' }}>State</Typography>
+          <Typography>{state}</Typography>
+        </Grid>
+      </Grid>
+      <Grid container item xs={12} sm={6}>
+        <Grid item xs={12}>
+          <Typography sx={{ fontWeight: 600, fontSize: '16px' }}>ZipCode</Typography>
+          <Typography>{zipCode}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography sx={{ fontWeight: 600, fontSize: '16px' }}>Country</Typography>
+          <Typography>{country}</Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
 interface SeeMoreCardProps {
   logoUrl: string;
   avatarSize: number;
@@ -44,10 +125,18 @@ interface SeeMoreCardProps {
   coverImageUrl: string;
   shortDescription: string;
   longDescription: string;
-  address: string;
+  address: string | undefined;
   url: string;
   slug: string;
+  ein: string;
 }
+const defaultData = {
+  street: undefined,
+  city: undefined,
+  state: undefined,
+  zipCode: undefined,
+  country: undefined,
+};
 
 const SeeMoreCard: React.FC<SeeMoreCardProps> = ({
   logoUrl,
@@ -59,7 +148,21 @@ const SeeMoreCard: React.FC<SeeMoreCardProps> = ({
   address,
   url,
   slug,
+  ein,
 }) => {
+  const [getHunterData, { data, loading }] = useLazyQuery(getOrgHunterCharityBasic);
+  const { street, city, state, zipCode, country } = data ? data.charityBasic : defaultData;
+
+  useEffect(() => {
+    if (ein) {
+      getHunterData({
+        variables: { ein: Number(ein) },
+      });
+    }
+  }, []);
+
+  // const { street, city, state, zipCode, country, assetAmount, form990 } = hunterData;
+
   setTimeout(() => {
     createWidget(slug);
   }, 0);
@@ -91,7 +194,7 @@ const SeeMoreCard: React.FC<SeeMoreCardProps> = ({
       <CardMedia image={coverImageUrl} sx={{ height: 273 }} />
       <CardContent>
         <Grid container sx={{ width: 1, height: 1 }}>
-          <Grid item xs={7} width={1} height={1} pr={2}>
+          <Grid item xs={12} md={7} width={1} height={1} pr={2}>
             <Typography sx={{ fontSize: '24px', fontWeight: 600, mb: 2 }}>Description</Typography>
             <Box
               width={1}
@@ -114,23 +217,42 @@ const SeeMoreCard: React.FC<SeeMoreCardProps> = ({
               <Typography>{longDescription}</Typography>
             </Box>
           </Grid>
-          <Grid item xs={5} pr={4}>
-            <Stack direction={'row'} alignItems={'center'} spacing={1}>
-              <Typography sx={{ fontWeight: 600 }}>Location</Typography>
-              <LocationOnIcon />
-            </Stack>
-            <Typography sx={{ mb: 2 }}>{address}</Typography>
-            <Stack direction={'row'} alignItems={'center'} spacing={1}>
+          <Grid item xs={12} md={5} pr={4}>
+            <SeemoreTabs>
+              {loading ? (
+                <div>Loading</div>
+              ) : data ? (
+                <>
+                  <FullLocation
+                    street={street}
+                    city={city}
+                    state={state}
+                    zipCode={zipCode}
+                    country={country}
+                  />
+                </>
+              ) : (
+                <PartialLocation address={'No Data'} />
+              )}
+            </SeemoreTabs>
+            {/* <Stack direction={'row'} alignItems={'center'} spacing={1}>
               <Typography sx={{ fontWeight: 600 }}>URL</Typography>
               <LinkIcon />
             </Stack>
             <Link component={RouterLink} to={url} target='_blank'>
               {url}
-            </Link>
+            </Link> */}
           </Grid>
         </Grid>
       </CardContent>
-      <CardActions sx={{ position: 'absolute', bottom: '2%', right: '2%' }}>
+      <CardActions
+        sx={{
+          position: { xs: 'relative', md: 'absolute' },
+          bottom: 0,
+          right: 0,
+          justifyContent: 'flex-end',
+        }}
+      >
         <Button id={`every-donate-${slug}`} href={`https://www.every.org/${slug}#/donate`}>
           Donate
         </Button>
@@ -152,17 +274,7 @@ const SeeMore: React.FC<SeeMoreProp> = ({ nonPropfit, slug }) => {
     variables: { name: addDash(nonPropfit) },
   });
 
-  const [getHunterData, { data: hunterData }] = useLazyQuery(getOrgHunterCharityBasic);
-
   const parsedData = data ? JSON.parse(data?.organization?.organization) : undefined;
-
-  useEffect(() => {
-    if (!loading && parsedData && parsedData.nonprofit && parsedData.nonprofit.ein) {
-      getHunterData({
-        variables: { ein: Number(parsedData.nonprofit.ein) },
-      });
-    }
-  }, [loading]);
 
   // we need error handlers here
   return (
@@ -171,7 +283,7 @@ const SeeMore: React.FC<SeeMoreProp> = ({ nonPropfit, slug }) => {
         parsedData ? (
           <SeeMoreCard
             logoUrl={parsedData.nonprofit.logoUrl}
-            avatarSize={70}
+            avatarSize={50}
             name={parsedData.nonprofit.name}
             coverImageUrl={parsedData?.nonprofit?.coverImageUrl}
             shortDescription={parsedData.nonprofit.description}
@@ -179,6 +291,7 @@ const SeeMore: React.FC<SeeMoreProp> = ({ nonPropfit, slug }) => {
             address={parsedData.nonprofit.locationAddress}
             url={parsedData.nonprofit.websiteUrl}
             slug={slug}
+            ein={parsedData?.nonprofit?.ein}
           />
         ) : (
           <FallBackState>
