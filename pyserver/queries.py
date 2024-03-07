b0f500d05df178ab2c485d75aee2b732fa9fff90
eddy zapata <ecz001@live.com>
2024-03-07 04:17:03 -0500

    fixing python server

--- pyserver/queries.py
+++ pyserver/queries.py
@@ -1,25 +1,12 @@
 from ariadne import convert_kwargs_to_snake_case
 from db import (client, collection)
 from bson.json_util import dumps
-import asyncio
-
-client.admin.command('ping')
-print("Pinged your deployment. You successfully connected to MongoDB!")
-current_user= collection.find_one({"name":"Eddy"})
-print(current_user)
-print('hello')
-
-testUser = {
-    "id": "ID",
-    "name": "String",
-    "lastName": "String",
-    "email":" String"
-}
 
 def getUser_resolver(obj, info):
     try:
+        current_user= collection.find_one({"name":"Eddy"})
         print(obj, info)
-        return testUser
+        return current_user
     except Exception as error:
         return error
 
@@ -27,17 +14,18 @@ def getUser_resolver(obj, info):
 def authenticateUser_resolver(obj, info, email, password):
     try:
         print(email, password, obj, info)
-        return testUser
+        current_user= collection.find_one({"name":"Eddy"})
+        return current_user
     except Exception as error:  # todo not found
         return error
     
 @convert_kwargs_to_snake_case
-async def createUser_resolver(obj, info, name, lastname, email, password):
+def createUser_resolver(obj, info, name, lastname, email, password, confirmpassword):
     try:
-        print(name, lastname, email, password, obj, info)
-        client
-        new_user= await  asyncio.collection.insert_one({"name": name, "lastName": lastname, "email": email, "password": password})
-        print('this', new_user)
-        return new_user
+        create_new_user = {"name": name, "lastname": lastname, "email": email, "password": password, "confirmpassword": confirmpassword}
+        new_user= collection.insert_one(create_new_user)
+        get_new_user = collection.find_one(create_new_user)
+        print(get_new_user)
+        return get_new_user
     except Exception as error:  # todo not found
         return error
